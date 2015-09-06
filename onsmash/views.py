9e6ef351da4926a6312c133b0a7548935c4519c4
Erik Cruz <erikc1989@live.com>
2015-09-06 02:43:19 +0000

    Add index page and route

--- onsmash/views.py
+++ onsmash/views.py
@@ -1,5 +1,12 @@
 from onsmash import app
 
+from flask import render_template, redirect, url_for
+
+
 @app.route("/")
 def index():
-    return "Welcome to my website!"
+    return redirect(url_for("videos"))
+
+@app.route("/videos")
+def videos():
+    return render_template("videos/index.html")
