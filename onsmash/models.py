9fbd3c5ae16c317891584075be893e85b9b20402
Erik Cruz <erikc1989@live.com>
2015-09-07 21:08:36 +0000

    Add one to many relationship between days and videos

--- onsmash/models.py
+++ onsmash/models.py
@@ -5,7 +5,16 @@ class Video(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     video_link = db.Column(db.String, nullable=False)
-    slug = db.Column(db.String)
+    slug = db.Column(db.String, nullable=False)
+    day_id = db.Column(db.Integer, db.ForeignKey("day.id"))
     
     def __repr__(self):
         return "<Video %s: %s>" % (self.id, self.title)
+
+class Day(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    videos = db.relationship("Video", backred="day",lazy="dynamic")
+    
+    def __repr__(self):
+        return "<Day %s: %s>" % (self.id, self.date)
