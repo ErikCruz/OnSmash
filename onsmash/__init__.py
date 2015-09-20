d55df07acf6ec76b493a4c2f0b5d4d682ad42321
Erik Cruz <erikc1989@live.com>
2015-09-20 01:01:52 +0000

    Enable file uploads

--- onsmash/__init__.py
+++ onsmash/__init__.py
@@ -8,7 +8,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["SECRET_KEY"] = "secret"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, 'onsmash.db')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, 'onsmash.db')
+
+app.config["UPLOAD_FOLDER"] = os.path.join(basedir, "thumb")
+app.config["ALLOWED_EXTENSIONS"] = set(['png', 'jpg', 'jpeg', 'gif'])
+
 db = SQLAlchemy(app)
 
 import views
--- onsmash/views.py
+++ onsmash/views.py
@@ -1,10 +1,16 @@
+import os
 import moment
 from onsmash import app, db
-from flask import render_template, redirect, url_for, request, flash
+from flask import render_template, redirect, url_for, request, flash, request
 from sqlalchemy import desc
 from forms import VideoForm
 from models import Day, Video
+from werkzeug import secure_filename
 
+# checked allowed extentions
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config["ALLOWED_EXTENSIONS"]
 
 @app.route("/")
 def index():
@@ -29,25 +35,30 @@ def embed(slug):
 def new_video():
     form = VideoForm()
     if form.validate_on_submit():
-        now = moment.now().format('dddd, MMMM D YYYY')
-        today = Day.query.filter_by(date=now).first()
-        if today is not None:
-            video = Video(title=form.title.data,description=form.description.data,video_link=form.video_link.data,day_id=today.id)
-            video.generate_slug()
-            db.session.add(video)
-            db.session.commit()
-            flash("Video Successfully Added")
-            return redirect(url_for("index"))
-        else:
-            day = Day(date=now)
-            db.session.add(day)
-            db.session.flush()
-            video = Video(title=form.title.data,description=form.description.data,video_link=form.video_link.data,day_id=day.id)
-            video.generate_slug()
-            db.session.add(video)
-            db.session.commit()
-            flash("Video Successfully Added")
-            return redirect(url_for("index"))
+        file = request.files["thumbnail"]
+        if allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        # now = moment.now().format('dddd, MMMM D YYYY')
+        # today = Day.query.filter_by(date=now).first()
+        # return request.files.get("file")
+        # if today is not None:
+        #     video = Video(title=form.title.data,description=form.description.data,video_link=form.video_link.data,day_id=today.id)
+        #     video.generate_slug()
+        #     db.session.add(video)
+        #     db.session.commit()
+        #     flash("Video Successfully Added")
+        #     return redirect(url_for("index"))
+        # else:
+        #     day = Day(date=now)
+        #     db.session.add(day)
+        #     db.session.flush()
+        #     video = Video(title=form.title.data,description=form.description.data,video_link=form.video_link.data,day_id=day.id)
+        #     video.generate_slug()
+        #     db.session.add(video)
+        #     db.session.commit()
+        #     flash("Video Successfully Added")
+        #     return redirect(url_for("index"))
     return render_template("videos/new.html",form=form)
 
 @app.errorhandler(404)
